534ba09ee471556747c21cc4401d6cbc22f18b15
Daniel Vlassov <danielvlassov@gmail.com>
2023-02-22 22:03:00 -0500

    Init (.h, main, README)

---
+++ GameOfLife.h
@@ -0,0 +1,16 @@
+#ifndef GAMEOFLIFE_H
+#define GAMEOFLIFE_H
+
+// GameOfLife Header File
+
+#include "Grid.h"
+
+class GameOfLife {
+public:
+    GameOfLife(int rows, int cols);
+    void run();
+private:
+    Grid m_grid;
+};
+
+#endif 
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "GameOfLife.h"
+
+int main() {
+    int rows, cols;
+    std::cout << "Enter the number of rows and columns for the grid: ";
+    std::cin >> rows >> cols;
+    // Pass to GameOfLife constructor
+    GameOfLife game(rows, cols);
+    game.run();
+    return 0;
+}
